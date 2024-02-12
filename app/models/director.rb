654892841e18b15217d4da71111146c7c3b1e9ef
David Le <43181047+DavidVLe1@users.noreply.github.com>
2024-02-12 01:25:07 +0000

    refactored code making use of methods in models.

--- app/models/director.rb
+++ app/models/director.rb
@@ -11,4 +11,23 @@
 #  updated_at :datetime         not null
 #
 class Director < ApplicationRecord
+  def filmography
+    my_id = self.id
+    matching_movies=Movie.where({:director_id => my_id})
+
+    return matching_movies
+  end
+
+  #three 1-n associations:
+  # -Director => Movie
+  # -Movie => Character
+  # -Actor =>Character
+
+  #Try define: 6 methods
+  # x Director#filmography
+  # - Movie#director
+  # - Movie#chracters
+  # - Character#movie
+  # - Actor#characters
+  # - Character#actor
 end
